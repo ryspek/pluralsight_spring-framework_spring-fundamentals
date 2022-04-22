360dfc1905560407df3cc51f1afd72b14848883a
Ryspek Baibosunov <ryspek.baybosunov@gmail.com>
2022-04-22 16:18:07 +0600

    add service

---
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -0,0 +1,16 @@
+package com.pluralsight.service;
+
+import com.pluralsight.model.Speaker;
+import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
+import com.pluralsight.repository.SpeakerRepository;
+
+import java.util.List;
+
+public class SpeakerServiceImpl {
+
+    private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+
+    public List<Speaker> findAll() {
+        return repository.findAll();
+    }
+}
