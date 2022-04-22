1e07e2bd01aa02b64c04c3e9d5aef3ebcd006e9f
Ryspek Baibosunov <ryspek.baybosunov@gmail.com>
2022-04-23 00:35:38 +0600

    add seedNum value and SpEL

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -11,13 +11,14 @@ public class Application {
 
         SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
 
-        System.out.println(service);
+        //System.out.println(service);
 
         System.out.println(service.findAll().get(0).getFirstName());
+        System.out.println(service.findAll().get(0).getSeedNum());
 
-        SpeakerService service2 = appContext.getBean("speakerService", SpeakerService.class);
+        //SpeakerService service2 = appContext.getBean("speakerService", SpeakerService.class);
 
-        System.out.println(service2);
+        //System.out.println(service2);
     }
 
 }
--- src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.pluralsight.repository;
 
 import com.pluralsight.model.Speaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -14,6 +15,9 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
     @Autowired
     private Calendar cal;
 
+    @Value("#{ T(java.lang.Math).random() * 100 }")
+    private double seedNum;
+
     public List<Speaker> findAll() {
         List<Speaker> speakers = new ArrayList<>();
 
@@ -21,6 +25,7 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
 
         speaker.setFirstName("Azamat");
         speaker.setLastName("Bakaev");
+        speaker.setSeedNum(seedNum);
 
         System.out.println("cal: " + cal.getTime());
 
