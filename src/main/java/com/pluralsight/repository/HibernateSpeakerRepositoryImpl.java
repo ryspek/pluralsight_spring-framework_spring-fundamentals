708e235cd70f19334d48c8e909d71faff2c988ed
Ryspek Baibosunov <ryspek.baybosunov@gmail.com>
2022-04-23 00:28:22 +0600

    add Factory bean

--- src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -1,13 +1,18 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Speaker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @Repository("speakerRepository")
 public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+
+    @Autowired
+    private Calendar cal;
 
     public List<Speaker> findAll() {
         List<Speaker> speakers = new ArrayList<>();
@@ -16,6 +21,8 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
 
         speaker.setFirstName("Azamat");
         speaker.setLastName("Bakaev");
+
+        System.out.println("cal: " + cal.getTime());
 
         speakers.add(speaker);
 
